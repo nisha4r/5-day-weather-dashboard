bb7a846080148f7c6b21f352ee429c5d5e925132
nisha <nisha.4r@gmail.com>
2023-04-16 22:04:50 -0600

    initializing variables in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,10 @@
 let cityEl = $('#city-input');
+let searchButtonEl = $('#search-button');
+let presentCityEl = $('#present-city');
+let  presentWeatherPicEl = $('#present-weather-pic');
+let tempEl = $('#temp');
+let windSpeedEl = $('#wind-speed');
+let humidityEl = $('#humidity');
 
 
 function load() {
