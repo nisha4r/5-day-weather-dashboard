4c950e48b6fd7b3fb6c96a656f455fc981366483
nisha <nisha.4r@gmail.com>
2023-04-16 22:09:45 -0600

    adding search history element

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,13 @@
 let cityEl = $('#city-input');
 let searchButtonEl = $('#search-button');
 let presentCityEl = $('#present-city');
-let  presentWeatherPicEl = $('#present-weather-pic');
+let presentWeatherPicEl = $('#present-weather-pic');
 let tempEl = $('#temp');
 let windSpeedEl = $('#wind-speed');
 let humidityEl = $('#humidity');
+let DayHeaderEl = $('#5-day-header');
+let searchHistoryEl = $('#search-history');
+
 
 
 function load() {
